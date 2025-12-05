c671f08fd02753547a49ad094730a2f82fe45f53
Seth007k <107913274+Seth007k@users.noreply.github.com>
2025-12-05 15:42:50 +0100

    added app index.js .gitignore

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App(){
+    return (
+        <div>
+            <h1>Project Y - Browsergame</h1>
+            <p> Willkommen zum frontend, hier entsteht bald eine grafik</p>
+        </div>
+    )
+}
+export default App;
