265eaf448b90812dcdb94b99cffc124a228594e1
Seth007k <107913274+Seth007k@users.noreply.github.com>
2025-12-18 12:18:30 +0100

    korrieigert

--- backend/src/services/Database.php
+++ backend/src/services/Database.php
@@ -2,10 +2,10 @@
 
 function getDB()
 { //getDB kann aufgerufen werden um eine verbindung zur datenbank zu bekommen
-    static $conn;
+    static $verbindung;
 
-    if (isset($conn)) { // existiert vie Verbindung bereits? wenn ja - reutrn; wen nein - erstelle neu
-        return $conn;
+    if (isset($verbindung)) { // existiert vie Verbindung bereits? wenn ja - reutrn; wen nein - erstelle neu
+        return $verbindung;
     }
 
     $config = require __DIR__ . '/../../config/env.php'; // lädt env.php und speichert in $env ; __DIR__ ist der ordner von database.php 
@@ -13,18 +13,18 @@ function getDB()
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Fehleraktivierung zum debuggen
 
     try {
-        $conn = new mysqli( //new msyqli(..) = verbindung php mit mysql 
+        $verbindung = new mysqli( //new msyqli(..) = verbindung php mit mysql 
             $config['DB_HOST'],
             $config['DB_USER'],
             $config['DB_PASS'],
             $config['DB_NAME'],
         );
-        $conn->set_charset('utf8mb4');
+        $verbindung->set_charset('utf8mb4');
     } catch (mysqli_sql_exception $e) {
         http_response_code(500);
-        echo json_encode(['erfolg' => false, 'fehler' => 'Die Verbindung zur datenbank ist fehlgeschlagen!', 'hinweis' => $e->getMessage()]);
+        echo json_encode(['erfolg' => false, 'fehler' => 'Die Verbindung zur Datenbank ist fehlgeschlagen!', 'hinweis' => $e->getMessage()]);
         exit;
     }
 
-    return $conn;
+    return $verbindung;
 }
